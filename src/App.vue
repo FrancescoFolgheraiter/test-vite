ac63c403c94741eb5f3264b456f42eff0b0b695c
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-17 13:23:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+/*
+    metodologia:
+    1)importazione componente;
+    2)dichiarazione del componente all'interno dell'oggetto
+    3)utilizzo del componente
+*/
+//1) importazione
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    data() {
+        return { count: 0 }
+    },
+    //2) Dichiarazione dei componenti
+    components: {
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <div>
+        <!--3)utilizzo del componente-->
+        <AppHeader/>
+        <h1>
+            Ciao ragazzi
+        </h1>
+        <p>
+            {{ count }}
+        </p>
+        <button @click="count++">
+            Incrementa count
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return { 
+            title:"Ciao ragazzi"
+        }
+    },
+};
+</script>
+
+<template>
+    <div>
+        <header>
+            <h1 class="main-title">
+                {{ title }}
+            </h1>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+    header{
+        padding:20px;
+        background-color: aquamarine;
+    }
+</style>
